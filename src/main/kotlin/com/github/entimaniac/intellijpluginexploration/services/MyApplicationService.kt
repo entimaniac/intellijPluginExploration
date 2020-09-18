3164aad28c0ae58a1644605f49a881358d0039bd
GitHub Action <action@github.com>
2020-09-18 18:22:12 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/entimaniac/intellijpluginexploration/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.entimaniac.intellijpluginexploration.services
+
+import com.github.entimaniac.intellijpluginexploration.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
